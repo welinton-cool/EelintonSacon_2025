c777d612d3155aec3af54391b2044960447dcc00
welinton-cool <welinton.sacon@espoch.edu.ec>
2025-05-08 17:16:53 -0500

    Analice la temperatura

---
+++ src/main/java/ec/edu/ec/welintonsacon_2025/WelintonSacon_2025.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.ec.welintonsacon_2025;
+
+/**
+ *
+ * @author HP
+ */
+public class WelintonSacon_2025 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
